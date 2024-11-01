5939789dc2848ad093182b1c824692d39214d3e6
k1d12r04 <k1d12r04@gmail.com>
2024-11-02 02:45:12 +0300

    Fetch products with pagination

--- app/components/product/Products.tsx
+++ app/components/product/Products.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Container, Typography, CircularProgress, Grid2 } from '@mui/material';
 import ProductItem from './ProductItem';
+import Pagination from '@mui/material/Pagination';
 
 export type Product = {
   id: number;
@@ -16,22 +17,31 @@ const Products: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 30;
+
+  const fetchProducts = async (page: number) => {
+    try {
+      const response = await axios.get('https://dummyjson.com/products', {
+        params: {
+          limit: itemsPerPage,
+          skip: (page - 1) * itemsPerPage,
+        },
+      });
+      setProducts(response.data.products);
+    } catch (error) {
+      setError('Failed to fetch products');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await axios.get('https://dummyjson.com/products');
-        setProducts(response.data.products);
-      } catch (error) {
-        setError('Failed to fetch products');
-      } finally {
-        setLoading(false);
-      }
-    };
+    fetchProducts(currentPage);
+    window.scrollTo(0, 0);
+  }, [currentPage]);
 
-    fetchProducts();
-  }, []);
-
+  const totalPages = Math.ceil(194 / itemsPerPage);
   if (loading) {
     return <CircularProgress />;
   }
@@ -42,15 +52,17 @@ const Products: React.FC = () => {
 
   return (
     <Container sx={{ mt: 4, mb: 4 }}>
-      <Grid2
-        container
-        spacing={2}
-        justifyContent={'center'}
-      >
+      <Grid2 container spacing={2} justifyContent={'center'}>
         {products.map(product => (
-          <ProductItem product={product} />
+          <ProductItem key={product.id} product={product} />
         ))}
       </Grid2>
+      <Pagination
+        count={totalPages}
+        page={currentPage}
+        onChange={(_, value) => setCurrentPage(value)}
+        sx={{ mt: 4, display: 'flex', justifyContent: 'center' }}
+      />
     </Container>
   );
 };
